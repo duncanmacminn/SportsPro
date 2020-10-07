4d0b539bd74a8fee7b05daa222b1d49442f20840
eggs2020 <e.gs@usask.ca>
2020-10-07 12:15:31 -0600

    Added view component

---
+++ SportsPro/ViewComponents/CopyrightViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsPro.Models;
+using SportsPro.DataLayer;
+
+namespace SportsPro.ViewComponents
+{
+    public class CopyrightViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var timeNow = DateTime.Now;
+
+            return View(timeNow);       
+        }
+    } 
+} 
